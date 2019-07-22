d28e3c66fc1bd346e93f0e9ec40d212a1bd24ba1
Kirill Yushkevich <kirill_yushkevich@epam.com>
2019-07-22 04:26:16 +0300

    Task1

---
+++ task01/ODataTask1Core/ODataTask1Core/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ODataTask1Core
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            NorthwindModel.NorthwindEntities entities = new NorthwindModel.NorthwindEntities(new Uri("https://services.odata.org/V3/Northwind/Northwind.svc"));
+            var taskFactory = new TaskFactory<IEnumerable<NorthwindModel.Customer>>();
+            var customers = (await taskFactory.FromAsync(entities.Customers.BeginExecute(null, null), (iar) => // breakpoint #2.1
+            {
+                return entities.Customers.EndExecute(iar); // breakpoint #2.2
+            })).ToArray();
+            Console.WriteLine("{0} customers in the service found.", customers.Length);
+            Console.ReadLine(); // breakpoint #2.3
+        }
+    }
+}
---
+++ task01/OdataTask1/OdataTask1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdataTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindModel.NorthwindEntities entities = new NorthwindModel.NorthwindEntities(new Uri("https://services.odata.org/V3/Northwind/Northwind.svc"));
+            var customers = entities.Customers.ToArray();
+            Console.WriteLine("{0} customers in the service found.", customers.Length);
+            Console.ReadKey();
+        }
+    }
+}
